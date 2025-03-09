5557aed0b16d884cf82f66accca44d660e17da0a
3x3ltv <makkanygin@gmail.com>
2025-03-09 17:15:59 +0300

    commit 4

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import paramiko
 import json
 
-# Переменные окружения
+
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 SFTP_HOST = os.getenv('SFTP_HOST')
-SFTP_PORT = os.getenv('SFTP_PORT')
+SFTP_PORT = int(os.getenv('SFTP_PORT', 2022))
 SFTP_USERNAME = os.getenv('SFTP_USER')
 SFTP_PASSWORD = os.getenv('SFTP_PASSWORD')
 WHITELIST_PATH = 'whitelist.json'
 
-# Создаем приложение
 application = Application.builder().token(TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -57,16 +56,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text(f'Ник {username} уже есть в вайтлисте или произошла ошибка.')
 
-# Регистрация обработчиков
 application.add_handler(CommandHandler("start", start))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-# Запуск вебхука
-port = int(os.environ.get("PORT", 8443))
-webhook_url = f"https://dulmine-bot.onrender.com/{TOKEN}"  # Замените на реальный URL после деплоя
-
-# Эта часть запускается только если скрипт запущен напрямую (не через gunicorn)
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8443))  # Render предоставляет порт через $PORT
+    webhook_url = f"https://dulmine-bot.onrender.com/{TOKEN}"
+    print(f"Starting webhook on port {port} with URL {webhook_url}")
     application.run_webhook(
         listen="0.0.0.0",
         port=port,
